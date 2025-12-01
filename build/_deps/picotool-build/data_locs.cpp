3acff5ceaf6691debbb20695f2093a7ab71ddda0
maborbaa <marciobar@gmail.com>
2025-12-01 19:07:49 -0300

    Criei o projeto com o FreeRTOS com testes no RP2040

---
+++ build/_deps/picotool-build/data_locs.cpp
@@ -0,0 +1,7 @@
+
+#include <vector>
+#include <string>
+
+std::vector<std::string> data_locs = {
+    "./","/home/tech03/Documentos/2025/EMBARCATECH/RESIDENCIA/MATERIAL_AULA/U7-projeto/PROJETO_FPGA/build/_deps/picotool/"
+};
