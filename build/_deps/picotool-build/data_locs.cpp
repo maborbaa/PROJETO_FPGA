c0f1cc604a9415a915a4d2642d2ed9ed72ebf24e
marcio <marciobar@gmail.com>
2025-12-01 19:19:56 -0300

    Delete build directory

--- build/_deps/picotool-build/data_locs.cpp
+++
@@ -1,7 +0,0 @@
-
-#include <vector>
-#include <string>
-
-std::vector<std::string> data_locs = {
-    "./","/home/tech03/Documentos/2025/EMBARCATECH/RESIDENCIA/MATERIAL_AULA/U7-projeto/PROJETO_FPGA/build/_deps/picotool/"
-};
